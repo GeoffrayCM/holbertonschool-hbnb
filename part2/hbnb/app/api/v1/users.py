2a8b883ccc87a2aa0ee18f2bb10bdd52ee42ddfa
Hedygh <dyouahes@gmail.com>
2026-03-12 16:12:40 +0100

    test

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -7,7 +7,8 @@ api = Namespace('users', description='User operations')
 user_model = api.model('User', {
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
+    'email': fields.String(required=True, description='Email of the user'),
+    'password': fields.String(required=True, description='Password of the user')
 })
 
 
@@ -20,18 +21,23 @@ class UserList(Resource):
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new user"""
-        user_data = api.payload
+        # create a copy of payload to store then escape password
+        user_data = dict(api.payload)
 
         # Simulate email uniqueness check (to be replaced by real validation with persistence)
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+        # get password value and remove it from dict to separate it from the rest (userdata to create user without psswd)
+        password = user_data.pop('password')
 
         try :
             new_user = facade.create_user(user_data)
+            # call hash method apart from create user
+            new_user.hash_password(password)
         except ValueError as e:
             return {"error": str(e)}, 400
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+        return {'id': new_user.id, 'message': 'User successfully created'}, 201
     
     @api.response(200, 'List of users retrieved successfully')
     def get(self):
@@ -52,6 +58,7 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
     
     @api.expect(user_model, validate=True)
     @api.response(200, 'User updated successfully')
@@ -71,3 +78,5 @@ class UserResource(Resource):
             "last_name": update_user.last_name,
             "email": update_user.email
         }, 200
+    
+    
